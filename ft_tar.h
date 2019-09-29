fa5651c9573f1e80d06af03def95d94be8342f18
Samuel Maddox <smaddox@e1z3r3p4.42.us.org>
2019-09-28 17:47:23 -0700

    archiving almost done

---
+++ ft_tar.h
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tar.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smaddox <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/28 06:43:15 by smaddox           #+#    #+#             */
+/*   Updated: 2019/09/28 15:51:40 by smaddox          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "ft_printf.h"
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+typedef struct		s_header_block
+{									
+									/*	OFFSETS	*/
+	char			name[100];		/*	0		*/
+	char			mode[8];		/*	100		*/
+	char			uid[8];			/*	108		*/
+	char			gid[8];			/*	116 	*/
+	char			size[12];		/*	124		*/
+	char			mtime[12];		/*	136		*/
+	char			chksum[8];		/*	148 	*/
+	char			typeflag;		/*	156		*/
+	char			linkname[100];	/*	157 	*/
+	char			magic[6];		/*	257		*/
+	char			version[2];		/*	263 	*/
+	char			uname[32];		/*	265		*/
+	char			gname[32];		/*	297		*/
+	char			devmajor[8];	/*	329		*/
+	char			devminor[8];	/*	337		*/
+	char			prefix[131];	/*	345		*/
+	char			atime[12];		/*	476		*/
+	char			ctime[12];		/*	488		*/
+	char			garbage[12];	/*	500		*/
+									/*	512		*/
+}					t_header_block;	
+
+typedef	struct		s_data_block
+{
+	unsigned char	data[512];
+}					t_data_block;
+
+/*	
+ * *	Both of union members are 512 bytes
+ * * *	so just write whatever is in each node's 
+ * *	union to the archive
+ */
+ 
+typedef	struct		s_write_block
+{
+	union				u_raw_block
+	{
+		t_header_block	header;
+		t_data_block	data;
+	}					u_data;	
+	struct s_write_block	*next;
+}							t_write_block;	
+
+
+void push_block(union u_raw_block new_block);
+void print_blocks(void);
+void print_filename(void);
+
---
+++ links.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   links.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smaddox <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/28 08:34:28 by smaddox           #+#    #+#             */
+/*   Updated: 2019/09/28 15:35:21 by smaddox          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_tar.h"
+
+t_write_block *head = NULL;
+t_write_block *tail = NULL;
+
+void	push_block(union u_raw_block new_block)
+{
+	if (!head && !tail)
+	{
+		head = (t_write_block*)malloc(sizeof(t_write_block));
+		tail = head;
+		memset(head, 0, sizeof(t_write_block));
+		tail->u_data = new_block;
+	}
+	else
+	{
+		tail->next = (t_write_block*)malloc(sizeof(t_write_block));
+		tail = tail->next;
+		memset(tail, 0, sizeof(t_write_block));
+		tail->u_data = new_block;
+	}
+}
+
+void	print_blocks(void)
+{
+	t_write_block *temp;
+	temp = head;
+	char *stepper;
+	int i;
+	while(temp)
+	{
+		i = -1;
+		stepper = (char*)temp;
+		while(++i < 512)
+			ft_printf("%02x", stepper[i]);
+		printf("\n");
+		temp = temp->next;
+	}
+}
+
+void	print_filename(void)
+{
+	int i = -1;
+	char *temp = (char*)head;
+	while (++i < 100)
+		ft_printf("%02x", temp[i]);
+}
+
+/*
+void	write_blocks(int fd)
+{
+	t_write_block *current = head;
+	while (current)
+	{
+		write(fd, current->u_data, 512);
+		current = current->next;
+	}
+}
+*/
